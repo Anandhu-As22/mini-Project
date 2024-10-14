03b9a231ddb65d2f4c930984ee7e23a7e4c89359
Anandhu-As22 <asanandhu2001@gmail.com>
2024-10-14 09:49:09 +0530

    almost done

--- wristwonders/wristwonders/settings.py
+++ wristwonders/wristwonders/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'NAME': 'wristwonders',
         'USER': 'mysuperuser',
         'PASSWORD': 'mysuperuser',
-        'HOST': 'wristwonders-db.cj64so4ga0ud.eu-north-1.rds.amazonaws.com',
+        'HOST': 'wristwonders.cj64so4ga0ud.eu-north-1.rds.amazonaws.com',
         'PORT': '5432'
     }
 }
@@ -193,3 +193,13 @@ RAZOR_KEY_ID ='rzp_test_86XtfvyElQrEXe'
 RAZOR_KEY_SECRET ='hjl1zuixHnMtl6Bap1HBwEnI'
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
+
+# AWS_ACCESS_KEY_ID = 'AKIA5V6I7DHXPH4GKIXW'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'wristwonderss'
+# AWS_S3_SIGNATURE_NAME = 's3v4'
+# AWS_S3_REGION_NAME = 'eu-north-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# AWS_S3_VERIFY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
